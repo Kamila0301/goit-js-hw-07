c941c0e4d5f35503d72ef8dc8065fd3a6e2749d3
Kamila0301 <bogda0301@gmail.com>
2023-09-12 17:23:52 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+createGallery();
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+  captions: true,
+});
+
+function createGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" width="800" height="600"/>
+   </a>
+</li>`;
+    })
+    .join("");
+  galleryList.insertAdjacentHTML("afterbegin", markup);
+}
